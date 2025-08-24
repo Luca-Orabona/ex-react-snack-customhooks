6edcea0454972884a79e5731895196870a0eceb0
Luca-Orabona <lucaorabona94@gmail.com>
2025-08-24 10:22:43 +0200

    feat: created hook useSwitch()

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import useSwitch from "./hook/useSwitch";
 
 function App() {
-  
+  const [isOn, toggle] = useSwitch();
+
 
   return (
     <div>
-      
+
+      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <button onClick={toggle}>Cambia Stato</button>
     </div>
   );
 }
